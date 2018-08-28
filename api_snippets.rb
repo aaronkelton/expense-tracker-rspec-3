31ea874da78147abd1676041657d1293163f3818
Aaron Kelton <aaron.k.kelton@gmail.com>
2018-08-28 17:48:34 -0500

    using dependency injection in the actual API class; ledger will need a record method? next we will mock the ledger object for use in test we don't need the actual Ledger class just yet; mock will suffice

--- api_snippets.rb
+++ api_snippets.rb
@@ -7,3 +7,10 @@ end
 
 # Later, callers do this:
 app = API.new(ledger: Ledger.new)
+
+# Pseudocode for what happens inside the API class:
+#
+result = @ledger.record({ 'some' => 'data' })
+result.success?      # => a Boolean
+result.expense_id    # => a number
+result.error_message # => a string or nil
