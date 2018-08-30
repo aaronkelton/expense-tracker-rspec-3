9779ec6425c361ac1667c35be813892605d262cb
Aaron Kelton <aaron.k.kelton@gmail.com>
2018-08-30 07:41:55 -0500

    specs for when expense fails validation

--- spec/unit/app/api_spec.rb
+++ spec/unit/app/api_spec.rb
@@ -14,6 +14,28 @@ module ExpenseTracker
     let(:ledger) { instance_double('ExpenseTracker::Ledger') }
 
     describe 'POST /expenses' do
+      context 'when the expense fails validation' do
+        let(:expense) { { 'some' => 'data' } }
+        before(:each) do
+          allow(ledger).to receive(:record)
+            .with(expense)
+            .and_return(RecordResult.new(false, 417, 'Expense incomplete'))
+        end
+
+        it 'returns an error message' do
+          post '/expenses', JSON.generate(expense)
+
+          parsed = JSON.parse(last_response.body)
+          expect(parsed).to include('error' => 'Expense incomplete')
+        end
+
+        it 'responds with a 422 (Unprocessable entity)' do
+          post '/expenses', JSON.generate(expense)
+
+          expect(last_response.status).to eq(422)
+        end
+      end
+
       context 'when the expense is successfully recorded' do
         let(:expense) { { 'some' => 'data' } }
         before(:each) do
@@ -21,6 +43,7 @@ module ExpenseTracker
             .with(expense)
             .and_return(RecordResult.new(true, 417, nil))
         end
+
 
         it 'returns the expense id' do
           post '/expenses', JSON.generate(expense)
