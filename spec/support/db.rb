05a87347b94d03b8f17f5f0fcd29f08011ffcc1a
Aaron Kelton <aaron.k.kelton@gmail.com>
2018-08-31 17:09:31 -0500

    db tag added to top level specs needing database rollbacks use  to conditionally require spec/db define db spec to always rollback when given db tag

--- spec/support/db.rb
+++ spec/support/db.rb
@@ -4,4 +4,10 @@ RSpec.configure do |c|
     Sequel::Migrator.run(DB, 'db/migrations')
     DB[:expenses].truncate
   end
+
+  c.around(:example, :db) do |example|
+    # either tag each example with :db that requires DB rollback fnality
+    # or use the RSpec configure in spec_helper for global fnality
+    DB.transaction(rollback: :always) { example.run }
+  end
 end
