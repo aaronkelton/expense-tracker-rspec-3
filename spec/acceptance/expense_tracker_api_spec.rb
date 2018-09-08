0f27fb3fab263b6b651c532e74887b184b0f4842
Aaron Kelton <aaron.k.kelton@gmail.com>
2018-09-08 08:54:08 -0500

    remove pending spec to have passing suite

--- spec/acceptance/expense_tracker_api_spec.rb
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -11,7 +11,6 @@ module ExpenseTracker
     end
 
     it 'records submitted expenses' do
-      pending 'Need to persist expenses'
       coffee = post_expense(
         'payee'  => 'Starbucks',
         'amount' => 5.75,
